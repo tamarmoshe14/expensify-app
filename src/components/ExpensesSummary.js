a997346f5b1ee06741a0ae59ee926fab9e6f1dc4
Tamar Moshe <tamarmoshe14@gmail.com>
2021-02-15 14:32:51 +0200

    styling

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import sumExpenses from '../selectors/sumExpenses';
 import selectExpenses from '../selectors/expenses';
 import numeral from 'numeral';
@@ -9,8 +10,13 @@ export const ExpensesSummary = (props) => {
     const expensesWord = props.expenseCount === 1 ? 'expense':'expenses' ;
     const formatedTotal = numeral(props.expenseTotal /100).format('$0,0.00')
     return (
-    <div>
-        <h3>Viewing {props.expenseCount} {expensesWord} totalling {formatedTotal}</h3>
+    <div className="page-header">
+        <div className="content-container">
+            <h1 className="page-header__title">Viewing <span>{props.expenseCount}</span> {expensesWord}, totalling <span>{formatedTotal}</span></h1>
+            <div className="page-header__actions">
+                <Link className="button" to="/create">Add Expense</Link>
+                </div>
+            </div>
     </div>
     )
 }
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -4,8 +4,13 @@ import {startLogin} from '../actions/auth'
 
 
 export const LoginPage = ({startLogin}) =>(
-    <div>
-    <button onClick={startLogin}>Login</button>
+    <div className="box-layout">
+    <div className="box-layout__box">
+        <h1 className="box-layout__title">Expensify</h1>
+        <p>Get Your Expenses Under Control</p>
+        <button className="button" onClick={startLogin}>Login With Google</button>
+    </div>
+    
     </div>
 );
 
